dee334caf8ed2bc72e345b35f08cf83b5b44c620
fhaag95 <florian@haag-mail.de>
2020-05-25 16:54:11 +0200

    Add sidtransform script

---
+++ sidtransform.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import struct
+import binascii
+import sys
+
+def format_struct_data(data):
+        return binascii.b2a_hex(data).decode("utf-8")
+
+def transform(sid):
+        sidhex = ""
+        numDashes = sid.count("-") - 1
+        parts = sid.split("-")
+
+        # revision
+        sidhex += format_struct_data(struct.pack(">B", int(parts[1])))
+        # number of dashes minus two
+        sidhex += format_struct_data(struct.pack(">B", numDashes))
+
+        sidhex += "{0:012x}".format(int(parts[2]))
+
+
+        for i in parts[3:]:
+                sidhex += format_struct_data(struct.pack("<I", int(i)))
+
+        print("SID: {}".format(sid))
+        print("SID in HEX: {}".format(sidhex.upper()))
+
+if __name__ == "__main__":
+        if len(sys.argv) < 2:
+                print("Usage: sidtransform.py <SID string>")
+                sys.exit()
+
+        transform(sys.argv[1])
