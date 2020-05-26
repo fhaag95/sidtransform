65f0762cb6a6d18c96decc16e0ea6706bcc70232
fhaag95 <florian@haag-mail.de>
2020-05-26 07:55:35 +0200

    Add cli to deal with user SIDs
    * Implement python click cli
    * Enhance README.md

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # SID Transform
-This repo contains a small script that can be used to transform a domain SID into its hex representation. 
+This repo contains a small script that can be used to transform a SID into its hex representation. 
 
-Given an example SID of *S-1-5-21-237813920-1760564327-87643179* the tool transforms this SID into its hex representation *010500000000000515000000A0C02C0E6714F0682B543905*
+Given an example domain SID of *S-1-5-21-237813920-1760564327-87643179* the tool transforms this SID into its hex representation *010500000000000515000000A0C02C0E6714F0682B543905*
 
-The script expects a domain SID, **not** an user SID. 
+The script is able to perform the transformation on domain SIDs and user SIDs.
 
 Transformations are done according to: https://devblogs.microsoft.com/oldnewthing/20040315-00/?p=40253
 
 ## Usage
 
-```sidtransform.py <SID string>```
+```
+Usage: sidtransform.py [OPTIONS] SID
+
+Options:
+  --sid-type [domain|user]  Specifies whether a domain SID or a user SID is
+                            supplied.  [required]
+  --help                    Show this message and exit.
+```
--- sidtransform.py
+++ sidtransform.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-import struct
-import binascii
-import sys
+import struct, binascii, sys, click
 
 def format_struct_data(data):
         return binascii.b2a_hex(data).decode("utf-8")
 
-def transform(sid):
+def transform(sid, dashReduction):
         sidhex = ""
         numDashes = sid.count("-") - 1
         parts = sid.split("-")
@@ -22,12 +20,21 @@ def transform(sid):
         for i in parts[3:]:
                 sidhex += format_struct_data(struct.pack("<I", int(i)))
 
+        return sidhex
+        
+
+@click.command()
+@click.option("--sid-type", type=click.Choice(["domain", "user"]), required=True, help="Specifies whether a domain SID or a user SID is supplied.")
+@click.argument("sid", )
+def start(sid_type, sid):
+        sid_hex = ""
+        if sid_type.lower() == "domain":
+                sid_hex = transform(sid, 1)
+        else:
+                sid_hex = transform(sid, 2)
+
         print("SID: {}".format(sid))
-        print("SID in HEX: {}".format(sidhex.upper()))
+        print("SID in HEX: {}".format(sid_hex.upper()))
 
 if __name__ == "__main__":
-        if len(sys.argv) < 2:
-                print("Usage: sidtransform.py <SID string>")
-                sys.exit()
-
-        transform(sys.argv[1])
+        start()
